62b1b700f50b755f6d4897dc23515c46313aa8b3
Arsonist Tefh <119712506+Arsonist-932@users.noreply.github.com>
2024-03-08 02:33:16 +0100

    Correction projects

--- src/components/ModalProject.jsx
+++ src/components/ModalProject.jsx
@@ -6,14 +6,14 @@ const ProjectModal = ({ onClose, project }) => {
   return (
     <>
       <div
-        className="fixed top-0 right-0 bottom-0 flex items-center justify-center z-50 bg-black bg-opacity-50 w-full text-black"
+        className="fixed top-0 right-0 bottom-0 flex items-center justify-center z-50 bg-black bg-opacity-50 w-full text-white"
         onClick={(e) => {
           onClose();
           e.stopPropagation();
         }}
       >
         <div
-          className="mx-6 p-4 min-w-[300px] md:max-w-[700px] h-[500px] sm:h-auto bg-white text-sm rounded-lg overflow-y-auto relative"
+          className="mx-6 p-4 py-8 min-w-[300px] md:max-w-[700px] h-[600px] sm:h-auto bg-black text-sm rounded-lg overflow-y-auto relative font-kaushan font-black"
           onClick={(e) => e.stopPropagation()}
         >
           <button>
@@ -24,27 +24,27 @@ const ProjectModal = ({ onClose, project }) => {
           </button>
 
           <div className="w-full">
-            <Image src={project.banner} alt="Banner" width={500} height={500} />
+            <Image src={project.banner} alt="Banner" width={700} height={500} />
           </div>
 
-          <div className="flex flex-col gap-4 my-4">
-            <div>
-              <h3 className="uppercase font-extrabold bg-gradient-to-br from-[#080808e5] to-primary-900 text-white text-center rounded-2xl mb-2">
+          <div className="flex flex-col gap-6 my-4">
+            <div className="font-kaushan">
+              <h3 className="uppercase font-extrabold text-xl text-red-600 text-center mb-2">
                 Présentation
               </h3>
-              <p className="px-3">{project.presentation}</p>
+              <p className="px-3 text-lg">{project.presentation}</p>
             </div>
             <div>
-              <h3 className="uppercase font-extrabold bg-gradient-to-br from-[#080808e5] to-primary-900 text-white text-center rounded-2xl mb-2">
+              <h3 className="mb-2 text-center uppercase font-extrabold text-xl text-red-600">
                 Objectif
               </h3>
-              <p className="px-3">{project.objectif}</p>
+              <p className="px-3 text-lg">{project.objectif}</p>
             </div>
 
             <div className="flex flex-wrap gap-2 mt-2 mb-3">
               {project.technologies.map((item, index) => (
                 <li key={index} className="list-none ">
-                  <p className="block px-3 py-2 bg-gradient-to-r from-primary-300 to-secondary-500 rounded-2xl text-white">
+                  <p className="block px-3 py-2  border border-red-500 rounded text-orange-600 cursor-default">
                     {item}
                   </p>
                 </li>
@@ -56,14 +56,14 @@ const ProjectModal = ({ onClose, project }) => {
             <Link
               href={project.gitUrl}
               target="_blank"
-              className="block px-5 py-2 bg-gradient-to-br from-[#080808e5] to-primary-900 rounded-xl text-white"
+              className="block px-5 py-2  border border-orange-500 bg-[#000000] text-red-600 rounded-xl hover:border-slate-800 hover:text-red-400"
             >
               Github
             </Link>
             <Link
               href={project.previewUrl}
               target="_blank"
-              className="block px-5 py-2 bg-gradient-to-br from-[#080808e5] to-primary-900 rounded-xl overflow-hidden text-white"
+              className="block px-5 py-2 border border-orange-500 bg-[#000000] text-red-600 rounded-xl hover:border-slate-800 hover:text-red-400"
             >
               Lien vers le site
             </Link>
